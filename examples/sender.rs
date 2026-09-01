46e1354a90b25fa4096056775d110851f1ece15e
Carson Weeks <mail@carsonweeks.com>
2026-08-31 21:06:40 -0400

    corrupting data

--- examples/sender.rs
+++ examples/sender.rs
@@ -3,7 +3,7 @@ use reliable_over_udp::ReliableSocket;
 
 fn main() -> anyhow::Result<()> {
     let addr = "0.0.0.0:34255".parse()?;
-    let socket = ReliableSocket::bind(addr)?;
+    let mut socket = ReliableSocket::bind(addr)?;
 
     let buf = std::fs::read("./alice.txt")?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use bytes::Bytes;
+use rand::prelude::*;
 use std::net::{SocketAddr, UdpSocket};
 
 // TODO: Add real error handling
@@ -14,54 +15,80 @@ enum SenderState {
 pub struct ReliableSocket {
     addr: SocketAddr,
     socket: UdpSocket,
+    sending_state: SenderState,
 }
 
 impl ReliableSocket {
     pub fn bind(addr: SocketAddr) -> Result<Self> {
         let socket = UdpSocket::bind(addr)?;
 
-        Ok(Self { addr, socket })
+        Ok(Self {
+            addr,
+            socket,
+            sending_state: SenderState::WaitForCall,
+        })
     }
 
-    pub fn send_to(&self, buf: &Bytes, addr: SocketAddr) -> Result<()> {
-        // Chop into MSS
+    pub fn send_to(&mut self, buf: &Bytes, addr: SocketAddr) -> Result<()> {
         let iter = buf.chunks(MSS_SIZE - 2);
 
-        // for each segment
+        self.sending_state = SenderState::WaitForAck;
         for chunk in iter {
-            // send segment
-            let checksum = generate_checksum(chunk);
-            let checksum_bytes = checksum.to_be_bytes();
+            self.send_chunk(addr, chunk)?;
 
-            let res: Vec<u8> = [&checksum_bytes[..], chunk].concat();
-            self.socket.send_to(&res, addr)?;
-            let mut ack_buffer = [0; MSS_SIZE];
-            let (_amt, src) = self.socket.recv_from(&mut ack_buffer)?;
-            println!("{}{}", checksum, str::from_utf8(&ack_buffer).unwrap());
-            // wait for ack
+            loop {
+                let mut ack_buffer = [0; MSS_SIZE];
+                let (amt, _src) = self.socket.recv_from(&mut ack_buffer)?;
+
+                let result = String::from_utf8_lossy(&ack_buffer[..amt]).to_string();
+                println!("{result}");
+                if result == "ACK" {
+                    break;
+                } else {
+                    self.send_chunk(addr, chunk)?;
+                }
+            }
         }
 
+        self.sending_state = SenderState::WaitForCall;
+        Ok(())
+    }
+
+    fn send_chunk(&self, addr: SocketAddr, chunk: &[u8]) -> Result<(), anyhow::Error> {\
+        // 50% chance for data to get corrupted.
+        let mut rng = rand::rng();
+        let random_number = rng.random_range(0..=1);
+
+        let checksum = generate_checksum(chunk);
+
+        let checksum = checksum + random_number;
+
+        let checksum_bytes = checksum.to_be_bytes();
+
+        let res: Vec<u8> = [&checksum_bytes[..], chunk].concat();
+        self.socket.send_to(&res, addr)?;
         Ok(())
     }
 
     pub fn recv_from(&self, buf: &mut [u8]) -> Result<()> {
-        // while true
-        // receive segment
-        // if segment matches checksum
-        //      ack
-        //      add to array
-        // if segment matches checksum
-        //      nack
-
         loop {
             let mut hold_buffer = [0; MSS_SIZE];
             let (amt, src) = self.socket.recv_from(&mut hold_buffer)?;
             let bytes = Bytes::copy_from_slice(&hold_buffer[..amt]);
+
+            let data = &bytes[2..];
             let checksum = &bytes[..2];
+
             let number = be_u8_to_u16(checksum);
-            println!("{}", validate_checksum(number, &bytes[2..]));
+
+            let valid_checksum = validate_checksum(number, data);
+            if valid_checksum {
+                self.socket.send_to(b"ACK", src)?;
+            } else {
+                self.socket.send_to(b"NACK", src)?;
+            }
+            println!("{}", validate_checksum(number, data));
             println!("{:?}", &bytes);
-            self.socket.send_to(b"ACK", src)?;
         }
 
         Ok(())
